c8a2a167b4bffd5f4a3b247fd2d4af39e3807c78
Kelly J Andrews <kelly@kellyjandrews.com>
2018-12-11 10:21:49 -0500

    updates from feedback

--- app/controllers/extend_controller.rb
+++ app/controllers/extend_controller.rb
@@ -7,7 +7,7 @@ class ExtendController < ApplicationController
     @extensions = document_paths.map do |document_path|
       document = File.read(document_path)
       frontmatter = YAML.safe_load(document)
-      next if frontmatter['published'] == false
+      next unless frontmatter['published']
       title = frontmatter['title']
       description = frontmatter['description']
       tags = frontmatter['tags'] || []
@@ -30,7 +30,7 @@ class ExtendController < ApplicationController
     description = frontmatter['description']
     tags = frontmatter['tags'] || []
     image = frontmatter['image'] || ''
-    cta = frontmatter['cta'] || ''
+    cta = frontmatter['cta'] || 'Use This'
     link = frontmatter['link'] || ''
     @extension = { title: title, body: body, image: image, description: description, tags: tags, link: link, cta: cta}
 
